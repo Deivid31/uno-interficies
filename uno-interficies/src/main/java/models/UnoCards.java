b5fa1ff7916e246dfd67be35cc748a1ae3b4901d
David Romero <david.ropi.2005@gmail.com>
2025-03-27 17:42:18 +0100

    Enums
    No se si se usarán

---
+++ uno-interficies/src/main/java/models/UnoCards.java
@@ -0,0 +1,20 @@
+package models;
+
+public class UnoCards {
+
+    public enum COLORS {
+        BLUE,
+        GREEN,
+        YELLOW,
+        RED,
+        BLACK
+    }
+
+    public enum POWERS {
+        BLOCK,
+        SWAP,
+        DRAW2,
+        CHANGE_COLOR,
+        DRAW4
+    }
+}
